fb4d4b37d24bf0d504cafc4216563e227340419c
gustavo-reinaldo <gf8370300@gmail.com>
2023-11-12 12:17:48 -0300

    Implementando o CREATE de Livro, e tratamento de exceções

--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Livro } from './livro.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,26 @@ export class LivroService {
 
   baseUrl: String = environment.baseUrl
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
 
   findAllByCategoria(id_cat: String): Observable<Livro[]> {
     const url = `${this.baseUrl}livros?categoria=${id_cat}`
     return this.http.get<Livro[]>(url)
   }
+
+  create (livro: Livro, id_cat: String): Observable<Livro> {
+    const url = `${this.baseUrl}livros?categoria=${id_cat}`
+    return this.http.post<Livro>(url, livro)
+  }
+
+
+  mensagem(str: String): void { 
+    this._snack.open(`${str}`, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000
+    })
+  }
+
 }
